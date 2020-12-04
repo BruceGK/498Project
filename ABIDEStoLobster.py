5d24a5627d27c4db065dcdf509c0d56c8adb2d33
LesterLian <lianzizhen@gmail.com>
2020-12-03 23:07:11 -0500

    Data: add main(); bug fix of tensor data type

--- ABIDEStoLobster.py
+++ ABIDEStoLobster.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import numpy as np
 import torch
+
 from tqdm import tqdm
+import argparse
+from glob import glob
+import os
 
 
-def abides_to_tensor(log_file='ORDERBOOK_ABM_FULL', output_file='orderbook.csv'):
+def abides_to_tensor(in_dir='../abides/log/rmsc03_two_hour/20191001', out_dir='data/F/long_data', idx=0):
     # Read log file
+    log_file = glob(os.path.join(in_dir, 'ORDERBOOK_*.bz2'))[0]
     df = pd.read_pickle(log_file)
     # df = df.head()
     progress = tqdm(total=len(df))
@@ -35,7 +40,7 @@ def abides_to_tensor(log_file='ORDERBOOK_ABM_FULL', output_file='orderbook.csv')
         # Get prices
         buy_prices, sell_prices = header[index][buys_index], header[index][sells_index]
         # Re-order and combine
-        orders = np.array(list(zip(buy_prices[:-11:-1], buys[:-11:-1], sell_prices[:10], sells[:10]))).ravel()
+        orders = np.array(list(zip(buy_prices[:-11:-1], abs(buys[:-11:-1]), sell_prices[:10], sells[:10]))).ravel()
         # Fill result array
         out_array[cur][0] = timestamp[i]
         out_array[cur][1:] = orders
@@ -43,9 +48,22 @@ def abides_to_tensor(log_file='ORDERBOOK_ABM_FULL', output_file='orderbook.csv')
         cur += 1
         progress.update()
 
-    t = torch.tensor(out_array, dtype=torch.float64)
+    t = torch.tensor(out_array).float()
     t[:, 1::2] *= 1e-4  # change prices to dollar ammounts
-    torch.save(t, 'data/F/long_data/00000000')
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    torch.save(t, f'{out_dir}/{idx:08}')
 
 
-abides_to_tensor('../abides/log/rmsc03_two_hour/ORDERBOOK_F_FREQ_10L.bz2')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert Abides logs into tensors.')
+
+    parser.add_argument('-l',
+                        '--log',
+                        required=True,
+                        help='Path to the directory with sub-directory containing logs for each day')
+    args = parser.parse_args()
+
+    paths = glob(os.path.join(args.log, '*'))
+    for i, path in enumerate(paths):
+        abides_to_tensor(path, idx=i)
